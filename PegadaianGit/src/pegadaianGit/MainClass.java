9fd207ab24aa4d1f52626a39f16fa69d8673609e
adhiakbar <57065754+adhiakbar@users.noreply.github.com>
2019-11-05 09:59:05 +0700

    version 500
    ok

---
+++ PegadaianGit/src/pegadaianGit/MainClass.java
@@ -0,0 +1,13 @@
+package pegadaianGit;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hell-o World!");
+		System.out.println("This is Version 500");
+
+	}
+
+}
